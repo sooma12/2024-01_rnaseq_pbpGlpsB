0dd18f6cca391f91ffc1e5be34343c12056f1762
soom12 <soo.m@northeastern.edu>
2024-01-31 11:32:58 -0500

    testing for trim

--- scripts/temp_test_shell_replace.sh
+++ scripts/temp_test_shell_replace.sh
@@ -4,7 +4,7 @@ echo $FASTQ_INDIR
 leftInSuffix="_R1_001.fastq"
 
 # Loop through all the left-read fastq files in INDIR
-for leftInFile in "$FASTQ_INDIR"*"$leftInSuffix"
+for leftInFile in ${FASTQ_INDIR}*${leftInSuffix}
 do
   echo "$leftInFile"
 done
