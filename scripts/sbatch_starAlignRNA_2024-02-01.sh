3c5ecc9096d0a4b57be98a6309721cc26fa9b422
sooma12 <soo.m@northeastern.edu>
2024-02-01 15:59:38 -0500

    updated AlignRNA STAR options

--- scripts/sbatch_starAlignRNA_2024-02-01.sh
+++ scripts/sbatch_starAlignRNA_2024-02-01.sh
@@ -4,6 +4,7 @@
 #SBATCH --time=04:00:00
 #SBATCH --array=1-9%10
 #SBATCH --ntasks=9
+#SBATCH --mem=100G
 #SBATCH --cpus-per-task=1
 #SBATCH --output=/work/geisingerlab/Mark/rnaSeq/2024-01_rnaseq_pbpGlpsB/slurm_logs/alignRNA.%N.%j.log
 #SBATCH --error=/work/geisingerlab/Mark/rnaSeq/2024-01_rnaseq_pbpGlpsB/slurm_logs/alignRNA.%N.%j.err
@@ -38,9 +39,14 @@ r2=$(sed -n "$SLURM_ARRAY_TASK_ID"p $SAMPLE_SHEET |  awk '{print $3}')
 echo "Running STAR on files $r1 and $r2"
 
 # STAR time
-echo STAR --runMode alignReads \
+# note alignIntronMax=1 to disallow introns for bacteria
+# BAM sorting seems to be memory intensive.  Set an available amount of memory using limitBAMsortRAM 
+STAR --runMode alignReads \
 --genomeDir $GENOME_DIR \
 --outSAMtype BAM SortedByCoordinate \
 --readFilesIn $r1 $r2 \
 --runThreadN 4 \
+--alignIntronMax 1 \
+--limitBAMsortRAM 5000000000 \
 --outFileNamePrefix ${OUT_DIR}/mapped/${name}
+
