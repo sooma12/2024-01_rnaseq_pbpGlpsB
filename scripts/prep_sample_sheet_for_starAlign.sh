1ec7de89d10d25d9e8b2e61d664e257e564d7359
soom12 <soo.m@northeastern.edu>
2024-02-01 15:37:47 -0500

    separated AlignRNA, adding a prep step to make sample_sheet.txt

---
+++ scripts/prep_sample_sheet_for_starAlign.sh
@@ -0,0 +1,28 @@
+# prep_sample_sheet_for_starAlign.sh
+# Makes a sample_sheet.txt containing sample ID and R1 and R2 filepaths
+# Example line:  WT_1 /path/to/WT_1_R1.fastq /path/to/WT_1_R2.fastq
+#
+
+FASTQ_DIR=/work/geisingerlab/Mark/rnaSeq/2024-01_rnaseq_pbpGlpsB/input/fastq
+SAMPLE_SHEET=${FASTQ_DIR}/sample_sheet.txt
+
+# Create .list files with R1 and R2 fastqs.  Sort will put them in same orders, assuming files are paired
+find $FASTQ_DIR -maxdepth 1 -name "*.fastq" | grep -e "_R1" | sort > R1.list
+find $FASTQ_DIR -maxdepth 1 -name "*.fastq" | grep -e "_R2" | sort > R2.list
+
+if [ -f "${SAMPLE_SHEET}" ] ; then
+  rm "${SAMPLE_SHEET}"
+fi
+
+# make sample sheet from R1 and R2 files.  Format on each line looks like (space separated):
+# WT_1 /path/to/WT_1_R1.fastq /path/to/WT_1_R2.fastq
+# from sample sheet, we can access individual items from each line with e.g. `awk '{print $3}' sample_sheet.txt`
+
+paste R1.list R2.list | while read R1 R2 ;
+do
+    outdir_root=$(echo "${R2}" | cut -f9 -d"/" | cut -f1,2 -d"_")
+    sample_line="${outdir_root} ${R1} ${R2}"
+    echo "${sample_line}" >> $SAMPLE_SHEET
+done
+
+rm R1.list R2.list
--- scripts/sbatch_starAlignRNA_2024-02-01.sh
+++ scripts/sbatch_starAlignRNA_2024-02-01.sh
@@ -27,26 +27,8 @@ SAMPLE_SHEET=${FASTQ_DIR}/sample_sheet.txt
 mkdir -p $OUT_DIR
 
 # Array job!  Used my sample sheet technique from 2023-11-13 breseq for this.
-# See https://www.biostars.org/p/449164/
-
-# Create .list files with R1 and R2 fastqs.  Sort will put them in same orders, assuming files are paired
-find $FASTQ_DIR -maxdepth 1 -name "*.fastq" | grep -e "_R1" | sort > R1.list
-find $FASTQ_DIR -maxdepth 1 -name "*.fastq" | grep -e "_R2" | sort > R2.list
-
-if [ -f "${SAMPLE_SHEET}" ] ; then
-  rm "${SAMPLE_SHEET}"
-fi
-
-# make sample sheet from R1 and R2 files.  Format on each line looks like (space separated):
-# WT_1 /path/to/WT_1_R1.fastq /path/to/WT_1_R2.fastq
-# from sample sheet, we can access individual items from each line with e.g. `awk '{print $3}' sample_sheet.txt`
-
-paste R1.list R2.list | while read R1 R2 ;
-do
-    outdir_root=$(echo "${R2}" | cut -f9 -d"/" | cut -f1,2 -d"_")
-    sample_line="${outdir_root} ${R1} ${R2}"
-    echo "${sample_line}" >> $SAMPLE_SHEET
-done
+# NOTE!!! sample sheet prep is moved to prep_sample_sheet_for_starAlign.sh.
+# Run that script before this one!
 
 # sed and awk read through the sample sheet and grab each whitespace-separated value
 name=$(sed -n "$SLURM_ARRAY_TASK_ID"p $SAMPLE_SHEET |  awk '{print $1}')
@@ -62,4 +44,3 @@ STAR --runMode alignReads \
 --readFilesIn $r1 $r2 \
 --runThreadN 4 \
 --outFileNamePrefix ${OUT_DIR}/mapped/${name}
-
